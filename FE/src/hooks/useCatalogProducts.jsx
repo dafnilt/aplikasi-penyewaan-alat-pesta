1f5703a9251f9a43b4b8ddb5007b0c3cc2d341a9
dafni <dafni.lanahtadya@gmail.com>
2026-06-08 23:44:28 +0700

    fix: format API Fate Time

--- FE/src/hooks/useCatalogProducts.jsx
+++ FE/src/hooks/useCatalogProducts.jsx
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { privateApi } from "../utils/axios.js";
+import { formatApiDateTime } from "../utils/formatApiDateTime.jsx";
 
 export function useCatalogProducts(startDate, endDate) {
   const guestId = localStorage.getItem("guestId") || "";
-
+  console.log("startDate", startDate);
+  console.log("endDate", endDate);
   const {
     data: products = [],
     isFetching,
@@ -17,11 +19,11 @@ export function useCatalogProducts(startDate, endDate) {
       const response = await privateApi.post(
         "/products/",
         {
-          startDate,
-          endDate,
+          startDate: formatApiDateTime(startDate),
+          endDate: formatApiDateTime(endDate),
           guestId,
         },
-        { signal }
+        { signal },
       );
 
       return response.data?.data ?? [];
