b98f8181b3d9ad1d330615bbbc3285bb46f9a4ce
Haya Qonita <qonitahaya@gmail.com>
2026-06-20 08:11:50 +0700

    FEAT: add checkbox style utils

---
+++ FE/src/utils/checkboxStyle.jsx
@@ -0,0 +1,17 @@
+export const checkboxStyle = {
+    p: 0,
+    color: "#1f1f1f",
+    "&.Mui-checked": {
+        color: "#1f1f1f",
+    },
+        "&:hover": {
+        backgroundColor: "transparent",
+    },
+
+    "&.Mui-focusVisible": {
+        backgroundColor: "transparent",
+    },
+    "& .MuiTouchRipple-root": {
+        display: "none",
+    },
+}
